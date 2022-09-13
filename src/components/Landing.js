c8ac396b651e358e70cc08fbf9aaaa40584685f9
Jacob Cole <jacobcole152@gmail.com>
2022-09-13 15:50:16 -0500

    Navbar and Landing Completed

---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar'
+
+const Landing = () => {
+  return (
+    <div id="#" className="frontImage">
+      <div className="container">
+        <Navbar />
+      </div>
+    </div>
+  )
+}
+
+export default Landing
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">About<span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Experience</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="blank" rel="noopener noreferrer" href="/Resume.pdf">Resume</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="blank" rel="noopener noreferrer" href="mailto: jacobcole152@gmail.com">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacob-cole-315399149/"><LinkedInIcon /></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="blank" rel="noopener noreferrer" href="https://github.com/jcolecode"><GitHubIcon /></a>
+                    </li>
+                </ul> 
+            </div>
+        </div>
+    </nav>
+)
+}
+
+export default Navbar
