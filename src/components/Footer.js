14a44a025f7b99bf5c809b51189297be838c67fd
Jacob Cole <jacobcole152@gmail.com>
2022-09-14 11:54:27 -0500

    Updated Website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import DescriptionIcon from '@mui/icons-material/Description';
 
 const Footer = () => {
   return (
@@ -10,10 +12,10 @@ const Footer = () => {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item">
-                                <a class="nav-link" target="blank" rel="noopener noreferrer" href="/Resume.pdf">Resume</a>
+                                <a class="nav-link" target="blank" rel="noopener noreferrer" href="/Resume.pdf"><DescriptionIcon /></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" target="blank" rel="noopener noreferrer" href="mailto: jacobcole152@gmail.com">Contact</a>
+                                <a class="nav-link" target="blank" rel="noopener noreferrer" href="mailto: jacobcole152@gmail.com"><EmailIcon /></a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" target="blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacob-cole-315399149/"><LinkedInIcon /></a>
