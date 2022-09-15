0a025ce8fbd95b6526ff62feb2d8a7d9fbdc3c13
Jacob Cole <jacobcole152@gmail.com>
2022-09-15 01:42:29 -0500

    Updated Website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
   return (
     <div class="footer">
                 <div class="d-flex flex-row my-4 ">
-                     <a class="nav-link mx-2" target="_blank" rel="noopener noreferrer" href="/Resume.pdf"><DescriptionIcon /></a>
-                     <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="mailto:jacobcole152@gmail.com"><EmailIcon /></a>
-                     <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacob-cole-315399149/"><LinkedInIcon /></a>
-                     <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="https://github.com/jcolecode"><GitHubIcon /></a>
+                     <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer" class="nav-link mx-2"><DescriptionIcon /></a>
+                     <a href="mailto:jacobcole152@gmail.com" target="_blank" rel="noopener noreferrer" class="nav-link mx-2"><EmailIcon /></a>
+                     <a href="https://www.linkedin.com/in/jacob-cole-315399149/" target="_blank" rel="noopener noreferrer" class="nav-link mx-2"><LinkedInIcon /></a>
+                     <a href="https://github.com/jcolecode" target="_blank" rel="noopener noreferrer" class="nav-link mx-2"><GitHubIcon /></a>
                 </div>
             
         <p>Jacob Cole © 2022</p>
