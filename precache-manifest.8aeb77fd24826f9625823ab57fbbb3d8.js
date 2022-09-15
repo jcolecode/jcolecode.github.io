b0f310bbf5f6f756e582293cf7ec7f1051bcaec0
Jacob Cole <jacobcole152@gmail.com>
2022-09-15 00:08:30 -0500

    Updates

---
+++ precache-manifest.8aeb77fd24826f9625823ab57fbbb3d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b561a3aaceb13600b37",
+    "url": "/portfolio-website/static/css/main.09e09147.chunk.css"
+  },
+  {
+    "revision": "0b561a3aaceb13600b37",
+    "url": "/portfolio-website/static/js/main.9070172f.chunk.js"
+  },
+  {
+    "revision": "0dc13a77a6af3a931488",
+    "url": "/portfolio-website/static/js/runtime~main.716f3bd7.js"
+  },
+  {
+    "revision": "35bce6ebc017c88b4079",
+    "url": "/portfolio-website/static/css/2.4659bcf9.chunk.css"
+  },
+  {
+    "revision": "35bce6ebc017c88b4079",
+    "url": "/portfolio-website/static/js/2.12dfeede.chunk.js"
+  },
+  {
+    "revision": "99a6e063b9000d961378",
+    "url": "/portfolio-website/static/js/3.187cc4d3.chunk.js"
+  },
+  {
+    "revision": "3a8e1edc1dfd66150e179c8dd0110edb",
+    "url": "/portfolio-website/static/media/landing.3a8e1edc.jpg"
+  },
+  {
+    "revision": "30aab96359ceae0bd65360c961e239b3",
+    "url": "/portfolio-website/index.html"
+  }
+];
