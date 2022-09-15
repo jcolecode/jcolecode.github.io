459fbbe7698dc7d428ffc81a0e7d47e60e2aa5c2
Jacob Cole <jacobcole152@gmail.com>
2022-09-15 01:35:39 -0500

    Updated Website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div class="footer">
                 <div class="d-flex flex-row my-4 ">
-                     <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="../Resume.pdf"><DescriptionIcon /></a>
+                     <a class="nav-link mx-2" target="_blank" rel="noopener noreferrer" href="/Resume.pdf"><DescriptionIcon /></a>
                      <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="mailto:jacobcole152@gmail.com"><EmailIcon /></a>
                      <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacob-cole-315399149/"><LinkedInIcon /></a>
                      <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="https://github.com/jcolecode"><GitHubIcon /></a>
