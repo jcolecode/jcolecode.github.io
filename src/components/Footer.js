5ad1c38861280b5b4b2f7eb02d9e7d269cc3fed1
Jacob Cole <jacobcole152@gmail.com>
2022-09-14 21:59:22 -0500

    Updated Website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,26 +7,13 @@ import DescriptionIcon from '@mui/icons-material/Description';
 const Footer = () => {
   return (
     <div class="footer">
-            <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-                <div class="container">
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav navbar-center">
-                            <li class="nav-item">
-                                <a class="nav-link" target="blank" rel="noopener noreferrer" href="/Resume.pdf"><DescriptionIcon /></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" target="blank" rel="noopener noreferrer" href="mailto: jacobcole152@gmail.com"><EmailIcon /></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" target="blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacob-cole-315399149/"><LinkedInIcon /></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" target="blank" rel="noopener noreferrer" href="https://github.com/jcolecode"><GitHubIcon /></a>
-                            </li>
-                        </ul> 
-                    </div>  
+                <div class="d-flex flex-row mb-2">
+                     <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="/Resume.pdf"><DescriptionIcon /></a>
+                     <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="mailto: jacobcole152@gmail.com"><EmailIcon /></a>
+                     <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jacob-cole-315399149/"><LinkedInIcon /></a>
+                     <a class="nav-link mx-2" target="blank" rel="noopener noreferrer" href="https://github.com/jcolecode"><GitHubIcon /></a>
                 </div>
-            </nav>
+            
         <p>Jacob Cole © 2022</p>
     </div>
   )
