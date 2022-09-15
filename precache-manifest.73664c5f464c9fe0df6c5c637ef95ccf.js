556f4ecc437f00990a0301250f93a74c5a5c91c8
Jacob Cole <jacobcole152@gmail.com>
2022-09-15 02:07:14 -0500

    Updates

---
+++ precache-manifest.73664c5f464c9fe0df6c5c637ef95ccf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fd87f3fbb6c768a2731",
+    "url": "/static/css/main.8ea2478e.chunk.css"
+  },
+  {
+    "revision": "8fd87f3fbb6c768a2731",
+    "url": "/static/js/main.72fca8f3.chunk.js"
+  },
+  {
+    "revision": "13f89a9950c3ecb65c1e",
+    "url": "/static/js/runtime~main.04cc8f18.js"
+  },
+  {
+    "revision": "be8e24b28788fa24c406",
+    "url": "/static/css/2.4659bcf9.chunk.css"
+  },
+  {
+    "revision": "be8e24b28788fa24c406",
+    "url": "/static/js/2.8f517471.chunk.js"
+  },
+  {
+    "revision": "19b810454e7d7a701ee7",
+    "url": "/static/js/3.b1c68d3b.chunk.js"
+  },
+  {
+    "revision": "0ee41dfdde7529df17b1d47ee65db56e",
+    "url": "/static/media/profile.0ee41dfd.jpg"
+  },
+  {
+    "revision": "f0e2615c5722d6d402712f78a0aea3fb",
+    "url": "/static/media/6.f0e2615c.JPEG"
+  },
+  {
+    "revision": "9a9ebf13507b042c79cbebbcda429daa",
+    "url": "/static/media/2.9a9ebf13.jpg"
+  },
+  {
+    "revision": "7976af16d9c1c8bdcb10ce1a327866e5",
+    "url": "/static/media/3.7976af16.jpg"
+  },
+  {
+    "revision": "23398373f77d5831262ae06c6f66b1bf",
+    "url": "/static/media/7.23398373.png"
+  },
+  {
+    "revision": "4d8c6b64689f6af7feb86a9e71805dd0",
+    "url": "/static/media/CS50.4d8c6b64.png"
+  },
+  {
+    "revision": "710fbad00d7a865c91cc0006de42d8c3",
+    "url": "/static/media/Lilly.710fbad0.png"
+  },
+  {
+    "revision": "3a8e1edc1dfd66150e179c8dd0110edb",
+    "url": "/static/media/landing.3a8e1edc.jpg"
+  },
+  {
+    "revision": "b3a2137b98df692d399fc58a22a2e276",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-website/precache-manifest.d1acb0e0a16f6640e418dbf1a288cbae.js"
+  "/precache-manifest.73664c5f464c9fe0df6c5c637ef95ccf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio-website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
